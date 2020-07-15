901f497a7bda83a0269365e362b434c82b1d9d19
DinuGarbuz <DinuGarbuz@users.noreply.github.com>
2020-07-15 21:04:03 +0300

    QuizManager and Quiz UI.

---
+++ OurFirstProject/Assets/Scripts/QuizManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<Questions> questions;
+
+    private Questions selectQuestion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectQuestion();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SelectQuestion()
+    {
+        int val;//= Random.Range(0, questions.Count);
+      //  selectedQuestion = questions[val];
+    }
+
+    void Answer()
+    {
+
+    }
+}
+
+[System.Serializable]
+
+public class Questions
+{
+    public string questionInfo;
+    public QuestionType questionType;
+    public Sprite questionImg;
+    public AudioClip questionClip;
+    public UnityEngine.Video.VideoClip questionVideo;
+    public List<string> options;
+    public string correctAns;
+
+}
+
+[System.Serializable]
+
+public enum QuestionType
+{
+    TEXT,
+    IMAGE,
+    VIDEO,
+    AUDIO
+}
---
+++ OurFirstProject/Assets/Scripts/QuizUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizUI : MonoBehaviour
+{
+
+    [SerializeField]  private Text questionText;
+    [SerializeField]  private Image questionImage;
+    [SerializeField]  private UnityEngine.Video.VideoPlayer questionVideo;
+    [SerializeField]  private AudioSource questionAudio;
+    [SerializeField] private List<Button> options;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
